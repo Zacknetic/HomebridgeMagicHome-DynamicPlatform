5376377bdb026076c18e069a8999dbf9209a6bb6
Lethegrin <zackavino@gmail.com>
2020-05-07 23:45:44 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import type { API } from 'homebridge';
+
+import { PLATFORM_NAME } from './settings';
+import { ExampleHomebridgePlatform } from './platform'; 
+
+/**
+ * This method registers the platform with Homebridge
+ */
+export = (api: API) => {
+  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+}
