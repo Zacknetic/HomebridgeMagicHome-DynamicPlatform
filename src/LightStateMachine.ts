bd8d3ccb73ee37ccc500d0eea5731d84c732a006
Igor Ramos <isramos@gmail.com>
2020-11-25 00:04:50 -0600

    state machine based on last send message

--- src/LightStateMachine.ts
+++ src/LightStateMachine.ts
@@ -1,19 +1,3 @@
-
-
-/*
-      Message Transmission Logic
-          Every time a new message arrives OR 100ms after last message, we perform the following logic:
-          CASE1: [scene] If have hue, sat and bri, then send message to device
-          CASE2: [hue/sat] else if I have only hue and sat, and idle for 100ms (bri missing), send message with last known bri
-          CASE3: [bri] else if I have bri, and 100ms has passed from last msg, then send msg
-          CASE4: [on/off] else if I have on/off, and 100ms has passed from last msg, then send msg
-          CASE5: [colorTemp]
-          CASEx: [error] else if I have only hue XOR sat, and 100ms has passed from last msg, then reset
-
-          After performing cases 1,2,3 update the target on state, if any.
-          -- Note: if in the future user reports issues, this is the likely cultript
-    */
-
 import { ILightState } from './magichome-interface/types';
 
 export interface ILightStateMachine {
@@ -24,50 +8,38 @@ export interface ILightStateMachine {
 /*  determine payload to be sent out */
 export class LightStateMachine{
 
-  static nextState(lightState: ILightState):ILightStateMachine{
-    const { targetOnState, targetBrightness, targetColorTemperature, targetHSL } = lightState.targetState;
-    const { hue, saturation } = targetHSL; 
+  static nextState(lightState: ILightState, lightLastState: ILightState):ILightStateMachine {
+    // lightState: has lastest and greatest user desired state, already reported to user
+    // lightLastState: has the last message we wrote to the lightbulb
+    // This functions outputs the next action based on the difference of those two states above
 
-    let message;
-    // check for simple off->on
-    if( targetOnState === true && hue === null && saturation === null && targetBrightness === null && targetColorTemperature === null ){
-      message = `toggle: on=${targetOnState}`;
-      return { nextState: 'toggleState', message};
-    }
-    
-    // check for simple on->off
-    if ( targetOnState === false && hue === null && saturation === null && targetBrightness === null && targetColorTemperature === null ){
-      message = `toggle: on=${targetOnState}`;
-      return { nextState: 'toggleState', message};
-    }
+    const { isOn: targetOnState, brightness:targetBrightness, colorTemperature: targetColorTemperature, HSL: targetHSL } = lightState;
+    const { hue:targetHue, saturation:targetSaturation } = targetHSL; 
 
-    if(lightState.isOn===false && !targetOnState){
-      message = `[BUG] Device is Off. Skip change, but keep state. hue:${hue} sat:${saturation} bri:${targetBrightness};`;
+    const { isOn: currentOnState, brightness: currentBrightness, colorTemperature: currentColorTemperature, HSL: currentHSL } = lightLastState;
+    const { hue:currentHue, saturation:currentSaturation } = currentHSL; 
+
+    let message = `on=${currentOnState}>${targetOnState} hue=${currentHue}>${targetHue} sat:${currentSaturation}>${targetSaturation} bri:${currentBrightness}>${targetBrightness}`;
+
+    if(currentOnState === targetOnState && 
+      currentHue === targetHue && 
+      currentSaturation === targetSaturation && 
+      currentBrightness === targetBrightness){
+      message = 'No Change Required - ' + message;
       return { nextState: 'keepState', message};
     }
-    
-    message = 'setColor.';
-    if(hue!==null){
-      lightState.HSL.hue = hue;
-      message += ` hue=${hue}*`;
-    } else {
-      message += ` hue=${lightState.HSL.hue}`;
+    if(currentOnState === false && targetOnState === false){
+      // light change while off, do not send to light and that would inadvertedly turn it on
+      message = 'Device is OFF. No change required. '  + message;
+      return { nextState: 'keepState', message};
     }
 
-    if(saturation!==null){
-      lightState.HSL.saturation = saturation;
-      message += ` sat=${saturation}*`;
-    } else {
-      message += ` sat=${lightState.HSL.saturation}`;
+    if(currentOnState === true &&  targetOnState === false){
+      // user wants lights off, disregard HSL
+      message = `toggle: on=${targetOnState}`;
+      return { nextState: 'toggleState', message};
     }
-
-    if(targetBrightness!==null){
-      lightState.brightness = targetBrightness;
-      message += ` bri=${targetBrightness}*`;
-    } else {
-      message += ` bri=${lightState.brightness}||${Math.round(lightState.HSL.luminance*2)}`;
-    }
-
+    // user want light on at a certain h/s/b    
     return { nextState: 'setColor', message};
   }
 }
