25540646b7e55213cce20abb0edc88944a54a872
Igor Ramos <isramos@gmail.com>
2020-11-27 11:59:21 -0600

    Update LightStateMachine.ts

--- src/LightStateMachine.ts
+++ src/LightStateMachine.ts
@@ -8,38 +8,42 @@ export interface ILightStateMachine {
 /*  determine payload to be sent out */
 export class LightStateMachine{
 
-  static nextState(lightState: ILightState, lightLastState: ILightState):ILightStateMachine {
-    // lightState: has lastest and greatest user desired state, already reported to user
-    // lightLastState: has the last message we wrote to the lightbulb
+  static nextState(targetState: ILightState, currentState: ILightState):ILightStateMachine {
+    // targetState: has lastest and greatest user desired state
+    // currentState: what we believe the current light state is. (you can pass either last read or last write)
     // This functions outputs the next action based on the difference of those two states above
 
-    const { isOn: targetOnState, brightness:targetBrightness, colorTemperature: targetColorTemperature, HSL: targetHSL } = lightState;
+    const { isOn: targetOnState, brightness:targetBrightness, colorTemperature: targetColorTemperature, HSL: targetHSL } = targetState;
     const { hue:targetHue, saturation:targetSaturation } = targetHSL; 
 
-    const { isOn: currentOnState, brightness: currentBrightness, colorTemperature: currentColorTemperature, HSL: currentHSL } = lightLastState;
+    const { isOn: currentOnState, brightness: currentBrightness, colorTemperature: currentColorTemperature, HSL: currentHSL } = currentState;
     const { hue:currentHue, saturation:currentSaturation } = currentHSL; 
 
     let message = `on=${currentOnState}>${targetOnState} hue=${currentHue}>${targetHue} sat:${currentSaturation}>${targetSaturation} bri:${currentBrightness}>${targetBrightness}`;
-
-    if(currentOnState === targetOnState && 
-      currentHue === targetHue && 
-      currentSaturation === targetSaturation && 
-      currentBrightness === targetBrightness){
-      message = 'No Change Required - ' + message;
+    const noHueSatBriChange = currentHue === targetHue &&  currentSaturation === targetSaturation && currentBrightness === targetBrightness;
+    if(currentOnState === targetOnState && noHueSatBriChange){
+      message = 'Same state. No Change: ' + message;
       return { nextState: 'keepState', message};
     }
     if(currentOnState === false && targetOnState === false){
       // light change while off, do not send to light and that would inadvertedly turn it on
-      message = 'Device is OFF. No change required. '  + message;
+      message = 'Device is OFF. No change: '  + message;
       return { nextState: 'keepState', message};
     }
 
+    if(currentOnState === false &&  targetOnState === true && noHueSatBriChange){
+      // user wants lights on, no HSB
+      message = `Toggle: on=${currentOnState}>${targetOnState}`;
+      return { nextState: 'toggleState', message};
+    }
     if(currentOnState === true &&  targetOnState === false){
       // user wants lights off, disregard HSL
-      message = `toggle: on=${targetOnState}`;
+      message = `Toggle: on=${currentOnState}>${targetOnState}`;
       return { nextState: 'toggleState', message};
     }
     // user want light on at a certain h/s/b    
     return { nextState: 'setColor', message};
   }
+
+
 }
