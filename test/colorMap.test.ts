ef945994dff7a908e03d8b5f1f55729ab12513dd
Igor Ramos <isramos@gmail.com>
2020-11-29 18:30:25 -0600

    Update colorMap.test.ts

--- test/colorMap.test.ts
+++ test/colorMap.test.ts
@@ -3,6 +3,16 @@
 import Common from '../dist/accessories/common';
 import { ILightState, opMode } from '../dist/magichome-interface/types';
 
+const props = {
+  config: {
+    'whiteEffects': {
+      'simultaniousDevicesColorWhite': true,
+      'colorWhiteThreshold': 10,
+      'colorWhiteThresholdSimultaniousDevices': 50,
+      'colorOffThresholdSimultaniousDevices': 5,
+    },
+  },
+};
 const state = {
   isOn: true,
   operatingMode: opMode.redBlueGreenMode,
@@ -19,17 +29,21 @@ function tester(){
   let counter = 0;
   for(let h=0; h<256; h++){
     for(let s=0; s<256; s++){
-      for(let b=0; b<256; b++){
+      for(let b=100; b<=100; b++){
         counter++;
         state.HSL =  { hue: h, saturation: s, luminance: 0 };
         state.brightness = b;
-        const copy: ILightState =  deepCopy(state);
-        const converted = Common.convertHSBtoRGBWW(copy, {config: {}});
-        const restored = Common.convertRGBWWtoHSB(converted, {config: {}});  
-        if(state.HSL.hue !== restored.HSL.hue || state.HSL.saturation !== restored.HSL.saturation || state.brightness !== restored.brightness){
+
+        const original: ILightState =  deepCopy(state);
+        const transient: ILightState =  deepCopy(state);
+        Common.convertHSBtoRGBWW(transient, props);
+        Common.convertRGBWWtoHSB(transient, props);  
+        
+
+        if(original.HSL.hue !== transient.HSL.hue || original.HSL.saturation !== transient.HSL.saturation || original.brightness !== transient.brightness){
           console.log(`Error test ${counter}, h:${h} s:${s} b:${b} state mismatch.`);
           console.log('input: ', state);
-          console.log('output: ', restored);
+          console.log('output: ', transient);
           console.log(`Expected output h:${h} s:${s} b:${b} state mismatch.`);
           process.exit(0);
         }
@@ -38,6 +52,7 @@ function tester(){
     }
   }
 }
+
 
 function tester2(){
   console.log('Testing RGB and back');
@@ -48,14 +63,16 @@ function tester2(){
       for(let b=0; b<256; b++){
         counter++;
         state.RGB =  { red:r, green:g, blue:b };
-        const copy: ILightState =  deepCopy(state);
-        const converted = Common.convertRGBWWtoHSB(copy, {config: {}});
-        const restored = Common.convertHSBtoRGBWW(converted, {config: {}});  
+
+        const original: ILightState =  deepCopy(state);
+        const transient: ILightState =  deepCopy(state);
+        Common.convertHSBtoRGBWW(transient, props);
+        Common.convertRGBWWtoHSB(transient, props);  
       
-        if(state.RGB.red !== restored.RGB.red || state.RGB.blue !== restored.RGB.blue || state.RGB.green !== restored.RGB.green){
+        if(state.RGB.red !== transient.RGB.red || state.RGB.blue !== transient.RGB.blue || state.RGB.green !== transient.RGB.green){
           console.log(`Error test ${counter}.`);
           console.log('input: ', state);
-          console.log('output: ', restored);
+          console.log('output: ', transient);
           console.log(`Expected output r:${r} g:${g} b:${b}.`);
           process.exit(0);
         }
@@ -73,14 +90,16 @@ function tester3(){
     for(let ww=0; ww<256; ww++){
       counter++;
       state.whiteValues =  { warmWhite:ww, coldWhite:cw };
-      const copy: ILightState =  deepCopy(state);
-      const converted = Common.convertRGBWWtoHSB(copy, {config: {}});
-      const restored = Common.convertHSBtoRGBWW(converted, {config: {}});  
+
+      const original: ILightState =  deepCopy(state);
+      const transient: ILightState =  deepCopy(state);
+      Common.convertHSBtoRGBWW(transient, props);
+      Common.convertRGBWWtoHSB(transient, props);  
       
-      if(state.whiteValues.coldWhite !== restored.whiteValues.coldWhite || state.whiteValues.warmWhite !== restored.whiteValues.warmWhite){
+      if(state.whiteValues.coldWhite !== transient.whiteValues.coldWhite || state.whiteValues.warmWhite !== transient.whiteValues.warmWhite){
         console.log(`Error test ${counter}.`);
         console.log('input: ', state);
-        console.log('output: ', restored);
+        console.log('output: ', transient);
         console.log(`Expected output cw:${cw} ww:${ww}.`);
         process.exit(0);
       }
