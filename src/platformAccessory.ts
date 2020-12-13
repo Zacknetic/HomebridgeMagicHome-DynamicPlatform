abdde9b407ac6a262bb7a15ddca498dfc6212460
zacknetic <zackavino@gmail.com>
2020-12-13 15:05:17 -0500

    include firmware 5 with "on command"

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -485,7 +485,7 @@ export class HomebridgeMagichomeDynamicPlatformAccessory {
         if (( !this.colorCommand) || !this.lightState.isOn){ //if no color command or a command to turn the light off
           await this.send(this.lightState.isOn ? COMMAND_POWER_ON : COMMAND_POWER_OFF); // set the power
         } else {
-          if((this.myDevice.controllerFirmwareVersion <= 4 && this.myDevice.controllerFirmwareVersion > 1) || this.myDevice.controllerFirmwareVersion == 8){ 
+          if((this.myDevice.controllerFirmwareVersion <= 5 && this.myDevice.controllerFirmwareVersion > 1) || this.myDevice.controllerFirmwareVersion == 8){ 
             await this.send( COMMAND_POWER_ON ); // set the power
           }
           await this.updateDeviceState(); // set color
